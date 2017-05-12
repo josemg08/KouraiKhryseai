6831d10ba0683ee7464f361a23a7c7cc19323ebe
josemg08 <josemg08@gmail.com>
2017-05-12 18:00:00 -0300

    Rethinking the structure.

---
+++ app/src/main/java/josegonzalez/hephaestus/kouraikhryseai/modules/ButtonModuleParser.java
@@ -0,0 +1,8 @@
+package josegonzalez.hephaestus.kouraikhryseai.modules;
+
+/**
+ * Created by josegonzalez on 12/05/17.
+ */
+
+public class ButtonModuleParser {
+}
---
+++ gsonparselib/src/main/java/utils/JsonUtils.java
@@ -0,0 +1,27 @@
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**___.
+ * Created by Jose Gonzalez
+ __.*/
+public class JsonUtils {
+
+    protected String getJsonFromFile(String JsonURL, Context context) throws IOException {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open(JsonURL);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return json;
+    }
+
+}
+//.___ End of JsonUtils __./
