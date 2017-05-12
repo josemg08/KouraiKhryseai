cb6d74a3c08cd15b8b655a0edc24c4b702fa6a97
josemg08 <josemg08@gmail.com>
2017-05-12 17:23:55 -0300

    Working on GsonParse library and component customizable view.

--- app/src/main/java/josegonzalez/hephaestus/kouraikhryseai/SandboxActivity.java
+++ app/src/main/java/josegonzalez/hephaestus/kouraikhryseai/SandboxActivity.java
@@ -1,14 +1,61 @@
 package josegonzalez.hephaestus.kouraikhryseai;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
-public class SandboxActivity extends AppCompatActivity{
+public class SandboxActivity extends AppCompatActivity implements View.OnTouchListener{
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sandbox);
+    }*/
+
+    private float posX;
+    private float posY;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = new Button(this);
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.activity_main);
+
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(500, 500);
+        layoutParams.leftMargin = 50;
+        layoutParams.topMargin = 50;
+        button.setLayoutParams(layoutParams);
+
+        button.setOnTouchListener(this);
+        constraintLayout.addView(button);
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                posX = view.getX() - event.getRawX();
+                posY = view.getY() - event.getRawY();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                view.animate()
+                        .x(event.getRawX() + posX)
+                        .y(event.getRawY() + posY)
+                        .setDuration(0)
+                        .start();
+                break;
+
+            default:
+                return false;
+        }
+
+        return true;
     }
 
 }
---
+++ app/src/main/java/josegonzalez/hephaestus/kouraikhryseai/modules/ButtonModule.java
@@ -0,0 +1,17 @@
+package josegonzalez.hephaestus.kouraikhryseai.modules;
+
+import android.content.Context;
+
+import josegonzalez.hephaesus.module.JsonModel;
+
+/**
+ * Created by josegonzalez on 12/05/17.
+ */
+
+public class ButtonModule extends JsonModel{
+
+    public ButtonModule(Context context) {
+        super(context);
+    }
+
+}
