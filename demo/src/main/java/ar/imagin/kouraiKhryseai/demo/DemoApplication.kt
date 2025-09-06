a452a6874296f365de9916151de748ae96e3be70
josemaria.gonzalez <5204629+josemg08@users.noreply.github.com>
2025-09-06 19:23:08 -0300

    Finished adding typography customization options to KThemeConfig, this will also respond to SW variables

--- demo/src/main/java/ar/imagin/kouraiKhryseai/demo/DemoApplication.kt
+++ demo/src/main/java/ar/imagin/kouraiKhryseai/demo/DemoApplication.kt
@@ -1,6 +1,8 @@
 package ar.imagin.kouraiKhryseai.demo
 
 import android.app.Application
+import androidx.compose.ui.text.font.FontFamily
+import androidx.core.content.res.ResourcesCompat
 import ar.imagin.kouraikhryseai.compose.ui.theme.KThemeConfig
 import ar.imagin.kouraikhryseai.compose.ui.theme.KThemeConfigHolder
 
@@ -8,8 +10,16 @@ class DemoApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        val lunnaFontFamily =
+            ResourcesCompat.getFont(this, R.font.lunna)?.let {
+                FontFamily(it)
+            } ?: FontFamily.Default
+
         KThemeConfigHolder.init(
-            config = KThemeConfig()
+            config = KThemeConfig(
+                fontFamily = lunnaFontFamily
+            )
         )
     }
 
